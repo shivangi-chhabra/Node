dcb3b448bc68a712c0d12436626bc2d7a7d73171
shivangi-chhabra <40178102+shivangi-chhabra@users.noreply.github.com>
2020-08-31 02:47:14 +0530

    express and http together to read a json file

---
+++ index.js
@@ -0,0 +1,23 @@
+var express = require('express')
+var http = require('http')
+var fs = require('fs')
+
+var app = express();
+var server = http.createServer(app);
+
+app.get('/', function(req, res) {
+    res.send('<h1>Express works!</h1>')
+})
+
+app.get('/task', function(req, res) {
+    fs.readFile('./db.json', function(err, data) {
+        // to send only error otherwise you can write res.send(data.toString())
+        var tasks = JSON.parse(data.toString()).tasks;
+        res.json(tasks)  
+    })
+})
+server.listen(3000, function() {
+
+    console.log('Listening to the server port: 3000')
+
+})
