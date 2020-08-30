10c7f53005c0055aad876cba098cae8e2291cca8
shivangi-chhabra <40178102+shivangi-chhabra@users.noreply.github.com>
2020-08-31 01:22:21 +0530

    http server creation and emit on events

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
-var calc = require('./calc.js')
 
-result = add(4,5)
 
-console.log("The output " + result)
+var http = require('http')
+var events = require('events')
+var url = require('url')
+
+var eventEmitter = new events.EventEmitter();
+
+
+var server = http.createServer(function(req, res) {
+    eventEmitter.emit('someone requested', 'TEST') // event emit
+    // setting content header
+    res.writeHead(200, ('Content-Type','text/html'))
+    var q = url.parse(req.url, true).query
+    var txt = q.year + " " + q.month
+    //send string to response
+    res.end(txt)
+})
+
+eventEmitter.on('someone requested', function (data) {
+    console.log('A request has been done on the server!', data)
+}) // Event Listener
+
+server.listen(3000, 'localhost', function() {
+    console.log('Server started on port : 3000')
+})
+
