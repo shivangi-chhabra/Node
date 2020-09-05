282ae08a133a00af6f34d8a8034f93943bf56bae
shivangi-chhabra <40178102+shivangi-chhabra@users.noreply.github.com>
2020-09-06 04:13:12 +0530

    signup post api

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 var express = require('express')
-var app = express();
+var bodyParser = require('body-parser') 
 var mongoose = require('mongoose')
 var User = require('./models/User')
 var db = require('./mysetup/myurl').myurl
+var app = express();
+
 var port = process.env.PORT || 3000
+
+app.use(bodyParser.urlencoded({ extended : false}));
+app.use(bodyParser.json())
 
 mongoose.connect(db)
         .then(()=>{
@@ -16,6 +21,23 @@ mongoose.connect(db)
 app.get('/', (req, res) =>{
     res.status(200).send('Hi welcome to the login and Signup API')
 })
+
+app.post("/signup", async (req, res) => {
+    var newUser = new User({
+        username : req.body.username,
+        email : req.body.email,
+        password : req.body.password
+    })
+    await newUser.save()
+             .then(()=>{
+                 res.status(200).send(newUser)
+             })
+             .catch(err => {
+                 console.log("Error is ", err.message)
+             })
+})
+
+
 
 app.listen(port, ()=>{
     console.log('Server is satarted on port ${port}')
