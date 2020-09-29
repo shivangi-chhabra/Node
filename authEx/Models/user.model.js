b9958e8ec4c72c908d116b34c83d465cfeca7d7f
shivangi-chhabra <40178102+shivangi-chhabra@users.noreply.github.com>
2020-09-30 00:44:08 +0530

    hashed password bcrypt

--- authEx/Models/user.model.js
+++ authEx/Models/user.model.js
@@ -2,6 +2,8 @@ const { Schema } = require('mongoose')
 const mongoose = require('mongoose')
 const schema   = mongoose.Schema
 
+const bcrypt = require('bcrypt')
+
 const userSchema = new Schema({
     email: {
         type: String,
@@ -14,6 +16,18 @@ const userSchema = new Schema({
         required: true
     }
 
+})
+
+userSchema.pre('save', async function (next) {
+    try {
+        //console.log('before saving')
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(this.password, salt)
+        this.password = hashedPassword
+        next()
+    } catch (error) {
+        next(error)
+    }
 })
 
 const tUser = mongoose.model('tuser', userSchema)
